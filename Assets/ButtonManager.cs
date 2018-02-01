3377a896e31297469f1cc722acebed9970d03e7b
Mary Li <ml4372@nyu.edu>
2018-02-01 10:39:34 -0500

    buttons

---
+++ Assets/ButtonManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour {
+
+	public static int slider = 0;
+	public static bool toggle, button;
+	public Button bt;
+	public Slider slide;
+	public Toggle tog;
+
+	void Start () {
+		slide.onValueChanged.AddListener(delegate {ValueChangeCheck(); });
+		slide.wholeNumbers = true;
+		slide.maxValue = 10;
+	}
+	
+	void Update () {
+		
+	}
+
+	public void ValueChangeCheck(){
+		Debug.Log (slide.value);
+	}
+}
