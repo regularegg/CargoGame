b5aaa848769218eb874bf377c05681ff23503b83
Mary Li <ml4372@nyu.edu>
2018-02-14 23:25:37 -0500

    bar graph representation
    for temperature (placeholder tho)

---
+++ Assets/GraphicalOutput.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphicalOutput : MonoBehaviour {
+	public GameObject temperature, humidity;
+	public float temperatureHold;
+	Transform temp, hum;
+
+	void Start () {
+		temp = temperature.transform;
+//		hum = humidity.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (temperatureHold != ShipStatKeeper.temperature) {
+			temperatureScale ();
+			temperatureHold = ShipStatKeeper.temperature;
+		}
+	}
+
+	void temperatureScale(){
+		temperature.transform.localScale = (ShipStatKeeper.temperature) * (Vector3.up/5)+(Vector3.right);
+		temperature.transform.localPosition = new Vector3(0, temperature.transform.localScale.y/10, 0);
+	}
+}
