bd51438a9b834d42a5b2f2184e8e553141d1538c
Mary Li <ml4372@nyu.edu>
2018-02-16 18:34:58 -0500

    Created Prefabs
    for selection screen, gameplay screen, some other stuff i dont rememebr

--- Assets/StateManager.cs
+++ Assets/StateManager.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class StateManager : MonoBehaviour {
+	public GameObject SelectionScreen;
+
 
 	public static int state;
+	public static int State{
+		get{ return state; }
+		set{ 
+			if (value == state) {
+				return;
+			} else if ((value != state)&&(value == 0)) {
+				state = value;
+				crewSelect ();
 
+			} else if ((value != state)&&(value == 1)) {
+				state = value;
+				startGame ();
+
+			}else if ((value != state)&&(value == 2)) {
+				state = value;
+			}
+		}
+	}
 	void Start () {
 		state = 0;
 	}
 	
 	void Update () {
-		
 	}
 
 	void shipSelect(){
@@ -22,11 +40,11 @@ public class StateManager : MonoBehaviour {
 
 	}
 
-	void crewSelect(){
+	static void crewSelect(){
 
 	}
 
-	void startGame(){
+	static void startGame(){
 
 	}
 }
