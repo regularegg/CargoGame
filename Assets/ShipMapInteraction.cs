f6573d6791dd9eba9ac15e5870ee2ea0abde3490
Mary Li <ml4372@nyu.edu>
2018-02-16 17:10:52 -0500

    Crew clickable and added tags
    nice

--- Assets/ShipMapInteraction.cs
+++ Assets/ShipMapInteraction.cs
@@ -18,9 +18,12 @@ public class ShipMapInteraction : MonoBehaviour {
 	void CastRay(){
 		Ray mapClick = Camera.main.ScreenPointToRay(Input.mousePosition);
 		RaycastHit2D hit = Physics2D.Raycast(mapClick.origin,mapClick.direction, Mathf.Infinity);
-		//Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero
 		if(hit.collider != null){
 			Debug.Log (" hit "+hit.collider.gameObject.name);
+			if (hit.collider.gameObject.tag.Contains("Crew")) {
+				Debug.Log (" hit Crew");
+			}
 		}
+
 	}
 }
